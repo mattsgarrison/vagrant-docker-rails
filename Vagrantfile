125b2cd9bf038849346420ac198020091ef606b1
Matt Garrison <mattsgarrison@gmail.com>
2013-08-30 16:08:13 -0500

    init repo

---
+++ Vagrantfile
@@ -0,0 +1,59 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # All Vagrant configuration is done here. The most common configuration
+  # options are documented and commented below. For a complete reference,
+  # please see the online documentation at vagrantup.com.
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "raring64"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/raring/current/raring-server-cloudimg-amd64-vagrant-disk1.box"
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  config.vm.network :forwarded_port, guest: 80, host: 8080
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+  config.vm.hostname = "rails.dev"
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  # config.vm.network :private_network, ip: "192.168.33.10"
+
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network :public_network
+
+  # If true, then any SSH connections made will enable agent forwarding.
+  # Default value: false
+  # config.ssh.forward_agent = true
+
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder. And the optional third
+  # argument is a set of non-required options.
+  config.vm.synced_folder "./docker", "/docker"
+
+  # Provider-specific configuration so you can fine-tune various
+  # backing providers for Vagrant. These expose provider-specific options.
+  # Example for VirtualBox:
+  #
+  config.vm.provider :virtualbox do |vb|
+  #   # Don't boot with headless mode
+    vb.gui = true
+  #
+  #   # Use VBoxManage to customize the VM. For example to change memory:
+    vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
+  
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = 'puppet/manifests'
+    puppet.module_path    = 'puppet/modules'
+  end
+
+end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,141 @@
+$ar_databases = ['activerecord_unittest', 'activerecord_unittest2']
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# --- PostgreSQL ---------------------------------------------------------------
+
+# class install_postgres {
+#   class { 'postgresql': }
+
+#   class { 'postgresql::server': }
+
+#   pg_database { $ar_databases:
+#     ensure   => present,
+#     encoding => 'UTF8',
+#     require  => Class['postgresql::server']
+#   }
+
+#   pg_user { 'rails':
+#     ensure  => present,
+#     require => Class['postgresql::server']
+#   }
+
+#   pg_user { 'vagrant':
+#     ensure    => present,
+#     superuser => true,
+#     require   => Class['postgresql::server']
+#   }
+
+#   package { 'libpq-dev':
+#     ensure => installed
+#   }
+
+#   package { 'postgresql-contrib':
+#     ensure  => installed,
+#     require => Class['postgresql::server'],
+#   }
+# }
+# class { 'install_postgres': }
+
+# --- Memcached ----------------------------------------------------------------
+
+class { 'memcached': }
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm/bin/rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # Thanks to @mpapis for this tip.
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --latest-binary --autolibs=enabled && rvm --fuzzy alias create default 2.0.0'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
+
+exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
+  creates => "${home}/.rvm/bin/bundle",
+  require => Exec['install_ruby']
+}
+
+# --- Docker -------------------------------------------------------------------
+
+exec { 'install_aufs':
+  command => "apt-get -y install linux-image-extra-`uname -r`",
+  require => Package['curl']
+}
+
+exec { 'install_docker_repo_key':
+  command => "sh -c 'curl http://get.docker.io/gpg | apt-key add -'",
+  require => Exec['install_aufs']
+}
+
+exec { 'install_docker_repo':
+  command => "sh -c 'echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list'",
+  require => Exec['install_docker_repo_key']
+}
+exec { 'update_from_docker_repo':
+  command => "apt-get update",
+    require => Exec['install_docker_repo']
+}
+
+exec { 'install_docker':
+  command => "apt-get install lxc-docker",
+  require => Exec['update_from_docker_repo']
+}
