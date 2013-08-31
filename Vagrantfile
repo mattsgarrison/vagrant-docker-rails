ce97214cfca6caca3b6c031ea85e21082f7d5fc9
Matt Garrison <matt@iconoclastlabs.com>
2013-08-31 13:28:04 -0500

    puppet will now install docker on a fresh machine

--- Vagrantfile
+++ Vagrantfile
@@ -10,8 +10,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "raring64"
-  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/raring/current/raring-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.box = "precise64"
+  #config.vm.box = "http://files.vagrantup.com/precise64_vmware.box"
+  #config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/raring/current/raring-server-cloudimg-amd64-vagrant-disk1.box"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
@@ -19,7 +20,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network :forwarded_port, guest: 80, host: 8080
   config.vm.network :forwarded_port, guest: 3000, host: 3000
 
-  config.vm.hostname = "rails.dev"
+  # config.vm.hostname = "rails.dev"
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
   # config.vm.network :private_network, ip: "192.168.33.10"
@@ -43,14 +44,22 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
   #
-  config.vm.provider :virtualbox do |vb|
-  #   # Don't boot with headless mode
-    vb.gui = true
-  #
-  #   # Use VBoxManage to customize the VM. For example to change memory:
-    vb.customize ["modifyvm", :id, "--memory", "1024"]
+  # config.vm.provider :virtualbox do |vb|
+  # #   # Don't boot with headless mode
+  # #  vb.gui = true
+  # #
+  # #   # Use VBoxManage to customize the VM. For example to change memory:
+  #   vb.customize ["modifyvm", :id, "--memory", "1024"]
+  # end
+
+  # vagrant up --provider=vmware_fusion
+  config.vm.provider "vmware_fusion" do |f|
+    f.vmx["memsize"] = "512"
+    f.vmx["numvcpus"] = "2"
+    f.vmx["displayName"] = "v_general"
+    f.vmx["annotation"] = "a general vagrant vm"
   end
-  
+
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = 'puppet/manifests'
     puppet.module_path    = 'puppet/modules'
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -13,13 +13,58 @@ stage { 'preinstall':
 }
 
 class apt_get_update {
+
+  exec { "update_apt-get":
+      command => "apt-get -y update"
+  }
+
+  package { 'apt-transport-https':
+    ensure => installed,
+    require => Exec['update_apt-get']
+  }
+
+  package { 'curl':
+    ensure => latest
+  }
+
+  exec { 'install_aufs':
+    command => "apt-get -y install linux-image-extra-`uname -r`",
+    require => Package['apt-transport-https']
+  }
+
+  exec { 'install_docker_repo_key':
+    command => "sh -c 'curl http://get.docker.io/gpg | apt-key add -'",
+    require => Exec['install_aufs']
+  }
+
+  exec { 'install_docker_repo':
+    command => "sh -c 'echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list'",
+    require => Exec['install_docker_repo_key']
+  }
+
   exec { 'apt-get -y update':
-    unless => "test -e ${home}/.rvm"
+    #unless => "test -e ${home}/.rvm",
+    require => Exec['install_docker_repo']
   }
 }
 class { 'apt_get_update':
   stage => preinstall
 }
+
+# --- Docker -------------------------------------------------------------------
+
+
+
+
+
+package { 'lxc-docker':
+  ensure => installed
+}
+# exec { 'install_docker':
+#   command => "apt-get update; apt-get install lxc-docker",
+#   require => Exec['install_docker_repo']
+# }
+
 
 # --- SQLite -------------------------------------------------------------------
 
@@ -68,10 +113,6 @@ class { 'memcached': }
 
 # --- Packages -----------------------------------------------------------------
 
-package { 'curl':
-  ensure => installed
-}
-
 package { 'build-essential':
   ensure => installed
 }
@@ -90,52 +131,50 @@ package { 'nodejs':
   ensure => installed
 }
 
+# Install zsh
+package { 'zsh':
+  ensure => installed
+}
+
+# Install byobu
+package { 'byobu':
+  ensure => installed
+}
+
+# Install zsh
+package { 'vim':
+  ensure => installed
+}
+
 # --- Ruby ---------------------------------------------------------------------
 
-exec { 'install_rvm':
-  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
-  creates => "${home}/.rvm/bin/rvm",
-  require => Package['curl']
+# exec { 'install_rvm':
+#   command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+#   creates => "${home}/.rvm/bin/rvm",
+#   require => Package['curl']
+# }
+
+# exec { 'install_ruby':
+#   # We run the rvm executable directly because the shell function assumes an
+#   # interactive environment, in particular to display messages or ask questions.
+#   # The rvm executable is more suitable for automated installs.
+#   #
+#   # Thanks to @mpapis for this tip.
+#   command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --latest-binary --autolibs=enabled && rvm --fuzzy alias create default 2.0.0'",
+#   creates => "${home}/.rvm/bin/ruby",
+#   require => Exec['install_rvm']
+# }
+
+# exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
+#   creates => "${home}/.rvm/bin/bundle",
+#   require => Exec['install_ruby']
+# }
+
+host { 'rails.dev':
+    ip => '127.0.0.1',
 }
 
-exec { 'install_ruby':
-  # We run the rvm executable directly because the shell function assumes an
-  # interactive environment, in particular to display messages or ask questions.
-  # The rvm executable is more suitable for automated installs.
-  #
-  # Thanks to @mpapis for this tip.
-  command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --latest-binary --autolibs=enabled && rvm --fuzzy alias create default 2.0.0'",
-  creates => "${home}/.rvm/bin/ruby",
-  require => Exec['install_rvm']
-}
-
-exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
-  creates => "${home}/.rvm/bin/bundle",
-  require => Exec['install_ruby']
-}
-
-# --- Docker -------------------------------------------------------------------
-
-exec { 'install_aufs':
-  command => "apt-get -y install linux-image-extra-`uname -r`",
-  require => Package['curl']
-}
-
-exec { 'install_docker_repo_key':
-  command => "sh -c 'curl http://get.docker.io/gpg | apt-key add -'",
-  require => Exec['install_aufs']
-}
-
-exec { 'install_docker_repo':
-  command => "sh -c 'echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list'",
-  require => Exec['install_docker_repo_key']
-}
-exec { 'update_from_docker_repo':
-  command => "apt-get update",
-    require => Exec['install_docker_repo']
-}
-
-exec { 'install_docker':
-  command => "apt-get install lxc-docker",
-  require => Exec['update_from_docker_repo']
+user { "vagrant":
+  ensure     => "present",
+  managehome => true,
 }
